e055880eeae9a21dbf4984c4b29f483b15987e1c
Akshith Rajesh <88131495+projectakshith@users.noreply.github.com>
2026-02-24 15:45:09 +0530

    Minimalist theme

--- src/components/AcademiaApp.tsx
+++ src/components/AcademiaApp.tsx
@@ -10,7 +10,7 @@ export default function AcademiaApp({
   customDisplayName,
   onUpdateName,
 }: any) {
-  const [theme, setTheme] = useState<"brutalist" | "minimalist">("brutalist");
+  const [theme, setTheme] = useState<"brutalist" | "minimalist">("minimalist");
   const academia = useAcademiaData(data);
 
   const sharedProps = {
